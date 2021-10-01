dd67115610e1d8eab7312a0e5514e59ef5a8aea3
Danny Stumpe <dannystumpe@gmail.com>
2021-10-02 01:04:48 +0200

    Refactored html

---
+++ utils.js
@@ -0,0 +1,257 @@
+const id_names = {
+    "left": {
+        "50000": {
+            "currencyUnit": "currencyUnit_left_50000",
+            "quantityUnit": "quantityUnit_left_50000",
+            "product": "product_left_50000",
+        },
+        "20000": {
+            "currencyUnit": "currencyUnit_left_20000",
+            "quantityUnit": "quantityUnit_left_20000",
+            "product": "product_left_20000",
+        },
+        "10000": {
+            "currencyUnit": "currencyUnit_left_10000",
+            "quantityUnit": "quantityUnit_left_10000",
+            "product": "product_left_10000",
+        },
+        "5000": {
+            "currencyUnit": "currencyUnit_left_5000",
+            "quantityUnit": "quantityUnit_left_5000",
+            "product": "product_left_5000",
+        },
+        "2000": {
+            "currencyUnit": "currencyUnit_left_2000",
+            "quantityUnit": "quantityUnit_left_2000",
+            "product": "product_left_2000",
+        },
+        "1000": {
+            "currencyUnit": "currencyUnit_left_1000",
+            "quantityUnit": "quantityUnit_left_1000",
+            "product": "product_left_1000",
+        },
+        "500": {
+            "currencyUnit": "currencyUnit_left_500",
+            "quantityUnit": "quantityUnit_left_500",
+            "product": "product_left_500",
+        },
+        "200": {
+            "currencyUnit": "currencyUnit_left_200",
+            "quantityUnit": "quantityUnit_left_200",
+            "product": "product_left_200",
+        },
+        "100": {
+            "currencyUnit": "currencyUnit_left_100",
+            "quantityUnit": "quantityUnit_left_100",
+            "product": "product_left_100",
+        },
+        "50": {
+            "currencyUnit": "currencyUnit_left_50",
+            "quantityUnit": "quantityUnit_left_50",
+            "product": "product_left_50",
+        },
+        "20": {
+            "currencyUnit": "currencyUnit_left_20",
+            "quantityUnit": "quantityUnit_left_20",
+            "product": "product_left_20",
+        },
+        "10": {
+            "currencyUnit": "currencyUnit_left_10",
+            "quantityUnit": "quantityUnit_left_10",
+            "product": "product_left_10",
+        },
+        "5": {
+            "currencyUnit": "currencyUnit_left_5",
+            "quantityUnit": "quantityUnit_left_5",
+            "product": "product_left_5",
+        },
+        "2": {
+            "currencyUnit": "currencyUnit_left_2",
+            "quantityUnit": "quantityUnit_left_2",
+            "product": "product_left_2",
+        },
+        "1": {
+            "currencyUnit": "currencyUnit_left_1",
+            "quantityUnit": "quantityUnit_left_1",
+            "product": "product_left_1",
+        },
+    },
+    "middle": {
+        "50000": {
+            "currencyUnit": "currencyUnit_middle_50000",
+            "quantityUnit": "quantityUnit_middle_50000",
+            "product": "product_middle_50000",
+        },
+        "20000": {
+            "currencyUnit": "currencyUnit_middle_20000",
+            "quantityUnit": "quantityUnit_middle_20000",
+            "product": "product_middle_20000",
+        },
+        "10000": {
+            "currencyUnit": "currencyUnit_middle_10000",
+            "quantityUnit": "quantityUnit_middle_10000",
+            "product": "product_middle_10000",
+        },
+        "5000": {
+            "currencyUnit": "currencyUnit_middle_5000",
+            "quantityUnit": "quantityUnit_middle_5000",
+            "product": "product_middle_5000",
+        },
+        "2000": {
+            "currencyUnit": "currencyUnit_middle_2000",
+            "quantityUnit": "quantityUnit_middle_2000",
+            "product": "product_middle_2000",
+        },
+        "1000": {
+            "currencyUnit": "currencyUnit_middle_1000",
+            "quantityUnit": "quantityUnit_middle_1000",
+            "product": "product_middle_1000",
+        },
+        "500": {
+            "currencyUnit": "currencyUnit_middle_500",
+            "quantityUnit": "quantityUnit_middle_500",
+            "product": "product_middle_500",
+        },
+        "200": {
+            "currencyUnit": "currencyUnit_middle_200",
+            "quantityUnit": "quantityUnit_middle_200",
+            "product": "product_middle_200",
+        },
+        "100": {
+            "currencyUnit": "currencyUnit_middle_100",
+            "quantityUnit": "quantityUnit_middle_100",
+            "product": "product_middle_100",
+        },
+        "50": {
+            "currencyUnit": "currencyUnit_middle_50",
+            "quantityUnit": "quantityUnit_middle_50",
+            "product": "product_middle_50",
+        },
+        "20": {
+            "currencyUnit": "currencyUnit_middle_20",
+            "quantityUnit": "quantityUnit_middle_20",
+            "product": "product_middle_20",
+        },
+        "10": {
+            "currencyUnit": "currencyUnit_middle_10",
+            "quantityUnit": "quantityUnit_middle_10",
+            "product": "product_middle_10",
+        },
+        "5": {
+            "currencyUnit": "currencyUnit_middle_5",
+            "quantityUnit": "quantityUnit_middle_5",
+            "product": "product_middle_5",
+        },
+        "2": {
+            "currencyUnit": "currencyUnit_middle_2",
+            "quantityUnit": "quantityUnit_middle_2",
+            "product": "product_middle_2",
+        },
+        "1": {
+            "currencyUnit": "currencyUnit_middle_1",
+            "quantityUnit": "quantityUnit_middle_1",
+            "product": "product_middle_1",
+        },
+    },
+    "right": {
+        "50000": {
+            "currencyUnit": "currencyUnit_right_50000",
+            "quantityUnit": "quantityUnit_right_50000",
+            "product": "product_right_50000",
+        },
+        "20000": {
+            "currencyUnit": "currencyUnit_right_20000",
+            "quantityUnit": "quantityUnit_right_20000",
+            "product": "product_right_20000",
+        },
+        "10000": {
+            "currencyUnit": "currencyUnit_right_10000",
+            "quantityUnit": "quantityUnit_right_10000",
+            "product": "product_right_10000",
+        },
+        "5000": {
+            "currencyUnit": "currencyUnit_right_5000",
+            "quantityUnit": "quantityUnit_right_5000",
+            "product": "product_right_5000",
+        },
+        "2000": {
+            "currencyUnit": "currencyUnit_right_2000",
+            "quantityUnit": "quantityUnit_right_2000",
+            "product": "product_right_2000",
+        },
+        "1000": {
+            "currencyUnit": "currencyUnit_right_1000",
+            "quantityUnit": "quantityUnit_right_1000",
+            "product": "product_right_1000",
+        },
+        "500": {
+            "currencyUnit": "currencyUnit_right_500",
+            "quantityUnit": "quantityUnit_right_500",
+            "product": "product_right_500",
+        },
+        "200": {
+            "currencyUnit": "currencyUnit_right_200",
+            "quantityUnit": "quantityUnit_right_200",
+            "product": "product_right_200",
+        },
+        "100": {
+            "currencyUnit": "currencyUnit_right_100",
+            "quantityUnit": "quantityUnit_right_100",
+            "product": "product_right_100",
+        },
+        "50": {
+            "currencyUnit": "currencyUnit_right_50",
+            "quantityUnit": "quantityUnit_right_50",
+            "product": "product_right_50",
+        },
+        "20": {
+            "currencyUnit": "currencyUnit_right_20",
+            "quantityUnit": "quantityUnit_right_20",
+            "product": "product_right_20",
+        },
+        "10": {
+            "currencyUnit": "currencyUnit_right_10",
+            "quantityUnit": "quantityUnit_right_10",
+            "product": "product_right_10",
+        },
+        "5": {
+            "currencyUnit": "currencyUnit_right_5",
+            "quantityUnit": "quantityUnit_right_5",
+            "product": "product_right_5",
+        },
+        "2": {
+            "currencyUnit": "currencyUnit_right_2",
+            "quantityUnit": "quantityUnit_right_2",
+            "product": "product_right_2",
+        },
+        "1": {
+            "currencyUnit": "currencyUnit_right_1",
+            "quantityUnit": "quantityUnit_right_1",
+            "product": "product_right_1",
+        },
+    },
+}
+
+document.getElementById("quantityUnit_right_500")
+    .addEventListener("keyup", function(event) {
+        let target_id = event.target.id
+
+        // get wrapper container of target formed as 
+        // "container_<left|middle|right>_<quantityUnit>"
+        let target_id_splitted = target_id.split('_').reverse()
+        let quantityUnit = target_id_splitted[0]
+        let columnSide = target_id_splitted[1]
+        let container_id = ["container", columnSide, quantityUnit].join("_")
+        
+        // get children of wrapper container
+        let children = document.querySelector('#' + container_id).children
+        let left_target = children[0]
+        let middle_traget = children[1]
+        let right_target = children[2]
+
+        let target = document.getElementById(target_id);
+        let label_left = document()
+        let qty_unit = target.value;
+        //let unit_size = 
+        document.getElementById("out500_right").innerHTML = 500 * parseFloat(qty_unit)
+});
