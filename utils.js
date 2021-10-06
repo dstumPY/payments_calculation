8d907fa7763db4d2275aa38a4251616660d19ff7
Danny Stumpe <dannystumpe@gmail.com>
2021-10-06 22:53:53 +0200

    Added function calc_product_on_keyup function

--- utils.js
+++ utils.js
@@ -232,26 +232,62 @@ const id_names = {
     },
 }
 
-document.getElementById("quantityUnit_right_500")
-    .addEventListener("keyup", function(event) {
-        let target_id = event.target.id
+const product_nodes = {
+    "left": document.querySelectorAll('.quantityUnit_left'),
+    "middle": document.querySelectorAll('.quantityUnit_middle'),
+    "right": document.querySelectorAll('.quantityUnit_right'),
+}
 
-        // get wrapper container of target formed as 
-        // "container_<left|middle|right>_<quantityUnit>"
-        let target_id_splitted = target_id.split('_').reverse()
-        let quantityUnit = target_id_splitted[0]
-        let columnSide = target_id_splitted[1]
-        let container_id = ["container", columnSide, quantityUnit].join("_")
+
+function calc_product_on_keyup(event) {
+    let target_id = event.target.id;
+
+    // get wrapper container of target formed as 
+    // "container_<left|middle|right>_<quantityUnit>"
+    let target_id_splitted = target_id.split('_').reverse();
+    let quantityUnit = target_id_splitted[0];
+    let columnSide = target_id_splitted[1];
+    let container_id = ["container", columnSide, quantityUnit].join("_");
+
+    // get children of wrapper container
+    let children = document.querySelector('#' + container_id).children;
+    let left_target = children[0];
+    let middle_traget = children[1];
+    let right_target = children[2];
+
+    // parse currencyUnit content as float
+    let currencyUnit = left_target.innerHTML.split('€')[0]
+    currencyUnit = parseFloat(currencyUnit.replace(",", "."));
+    let quantity = parseFloat(middle_traget.value);
+
+    // get product by multiplying currencyUnit with quantity
+    right_target.innerHTML = (currencyUnit * quantity).toFixed(2);
+
+    // get column container of form 'box_<left|middle|right>' in order
+    // to find all child nodes of class 'product_<left|middle|right>
+    let column_container = event.path[2];
+    let product_nodes = column_container.querySelectorAll(
+        '.product_left, .product_middle, .product_right'
+        );
         
-        // get children of wrapper container
-        let children = document.querySelector('#' + container_id).children
-        let left_target = children[0]
-        let middle_traget = children[1]
-        let right_target = children[2]
+        let sum = 0;
+        for (let i = 0; i < 15; i++) {
+            sum += parseFloat(product_nodes[i].innerHTML)
+        }
+        
+    // write result
+    let product_sum_container = column_container.querySelectorAll(
+        '#complete_sum_left, #complete_sum_middle, #complete_sum_right'
+        );
+    product_sum_container[0].innerHTML = sum
+}
 
-        let target = document.getElementById(target_id);
-        let label_left = document()
-        let qty_unit = target.value;
-        //let unit_size = 
-        document.getElementById("out500_right").innerHTML = 500 * parseFloat(qty_unit)
-});
+// add eventListener to all right column input nodes
+
+for (let x of product_nodes["left"]){
+    x.addEventListener("keyup", function (event) { calc_product_on_keyup(event) })
+}
+
+for (let x of product_nodes["right"]){
+    x.addEventListener("keyup", function (event) { calc_product_on_keyup(event) })
+}
